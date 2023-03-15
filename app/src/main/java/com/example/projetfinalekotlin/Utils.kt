23e037a39f05edc6a7227c36e5d7531496df97e8
wwwazz <wwwazz333@gmail.com>
2023-03-15 11:15:17 +0100

    calculate Distance Between 2 pts

--- app/src/main/java/com/example/projetfinalekotlin/Utils.kt
+++ app/src/main/java/com/example/projetfinalekotlin/Utils.kt
@@ -1,24 +1,33 @@
 package com.example.projetfinalekotlin
 
-import com.beust.klaxon.*
+import com.beust.klaxon.JsonReader
+import com.beust.klaxon.Klaxon
 import com.example.projetfinalekotlin.country.Countries
 import com.example.projetfinalekotlin.country.Country
+import com.google.android.gms.maps.model.LatLng
 import java.io.StringReader
-
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 
 object Utils {
     fun getJsonFromKlaxon(): ArrayList<Country> {
         val klaxon = Klaxon()
         val countriesArray = arrayListOf<Country>()
-        JsonReader(StringReader(Countries.countries)).use {
-                reader -> reader.beginArray {
-                    while (reader.hasNext()) {
-                        val product = klaxon.parse<Country>(reader)
-                        countriesArray.add(product!!)
-                    }
+        JsonReader(StringReader(Countries.countries)).use { reader ->
+            reader.beginArray {
+                while (reader.hasNext()) {
+                    val product = klaxon.parse<Country>(reader)
+                    countriesArray.add(product!!)
                 }
+            }
         }
         return countriesArray
+    }
+
+    fun calculateDistance(latLngClick: LatLng, latLngCity: LatLng): Double {
+        val distLat = latLngClick.latitude - latLngCity.latitude
+        val distLong = latLngClick.longitude - latLngCity.longitude
+        return sqrt(distLat.pow(2.0) + distLong.pow(2.0))
     }
 }
